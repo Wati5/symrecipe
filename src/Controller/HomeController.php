393b34b183f2bc1b9a818c0362f72430e7689e6f
Wati5 <Antoine_Barchon@etu.u-bourgogne.fr>
2024-06-26 14:36:29 +0200

    Partie 2 : CRUD des ingrédients - Formulaire pour créer un ingrédient (Personnalisation du rendu et message flash)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,15 +1,15 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class HomeController extends AbstractController
 {
-    #[Route('/', 'home.index', methods: ['GET'])]
+    #[Route('/', name: 'home.index', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('pages/home.html.twig');
     }
 }
